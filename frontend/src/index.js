608db3c2d501748cd07e7ff572bfd29979777600
D4-CTM <josuedelcid325@gmail.com>
2024-05-27 10:13:23 -0600

    first commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router } from "react-router-dom";
-import { AppProvider } from "./context";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <AppProvider>
-        <App />
-      </AppProvider>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+import App from './App';
 
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById('root'));
