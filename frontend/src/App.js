3eefaa966d186983038f8ac75091831573c3ae52
D4-CTM <josuedelcid325@gmail.com>
2024-05-27 00:19:12 -0600

    rebirth 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,33 +10,25 @@ class App extends React.Component {
   state = {
     data: {},
     country: '',
-    error: null,
   }
 
   async componentDidMount() {
-    try {
-      const data = await fetchData();
-      this.setState({ data, error: null });
-    } catch (error) {
-      this.setState({ error: error.message });
-    }
+    const data = await fetchData();
+
+    this.setState({ data });
   }
 
   handleCountryChange = async (country) => {
-    try {
-      const data = await fetchData(country);
-      this.setState({ data, country, error: null });
-    } catch (error) {
-      this.setState({ error: error.message });
-    }
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
   }
 
   render() {
-    const { data, country, error } = this.state;
+    const { data, country } = this.state;
 
     return (
       <div className={styles.container}>
-        {error && <div className={styles.error}>{error}</div>}
         <img className={styles.image} src={image} alt="COVID-19" />
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
